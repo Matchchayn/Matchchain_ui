99350f998b8880693120a9be16b88ad1133c8c4c
NileDex <152003924+NileDex@users.noreply.github.com>
2026-02-25 06:08:12 +0100

    chore: ignore frontend env and set IS_LOCAL to false

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -3,7 +3,7 @@
 // ==========================================
 // Set this to TRUE if you are running the backend locally on your computer.
 // Set this to FALSE if you want to connect to the live production server (Render).
-export const IS_LOCAL = true;
+export const IS_LOCAL = false;
 
 // The app will automatically use the correct URL based on your switch above!
 export const API_BASE_URL = IS_LOCAL
