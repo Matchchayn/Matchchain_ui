9543045a397a6959b47f14b2d641c03baeb1db60
NileDex <152003924+NileDex@users.noreply.github.com>
2026-03-03 03:11:11 +0100

    fix frontend usage

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -3,7 +3,7 @@
 // ==========================================
 // Set this to TRUE if you are running the backend locally on your computer.
 // Set this to FALSE if you want to connect to the live production server (Railway).
-export const IS_LOCAL = true; // Dynamically switches!
+export const IS_LOCAL = false; // Dynamically switches!
 
 // The app will automatically use the correct URL based on your switch above!
 export const API_BASE_URL = IS_LOCAL
